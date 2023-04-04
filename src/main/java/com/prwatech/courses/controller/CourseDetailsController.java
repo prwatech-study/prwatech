781c5bda1ea04a0a30acaefb2c7f3c78fd3b60f8
Manish <manishsinghonline2@gmail.com>
2023-04-05 00:57:31 +0530

    Manish | development | get course details added.

--- src/main/java/com/prwatech/courses/controller/CourseDetailsController.java
+++ src/main/java/com/prwatech/courses/controller/CourseDetailsController.java
@@ -1,14 +1,17 @@
 package com.prwatech.courses.controller;
 
 import com.prwatech.courses.dto.CourseCardDto;
+import com.prwatech.courses.model.CourseDetails;
 import com.prwatech.courses.service.CourseDetailService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import java.util.List;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,4 +78,24 @@ public class CourseDetailsController {
   public void getHomeListingFreeCourses() {}
 
   // get free online courses list paginated data.
+
+  @ApiOperation(
+      value = "Get course details by course id",
+      notes = "Get Get course details by course id")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Success"),
+        @ApiResponse(code = 400, message = "Not Available"),
+        @ApiResponse(code = 401, message = "UnAuthorized"),
+        @ApiResponse(code = 403, message = "Access Forbidden"),
+        @ApiResponse(code = 404, message = "Not found"),
+        @ApiResponse(code = 422, message = "UnProcessable entity"),
+        @ApiResponse(code = 500, message = "Internal server error"),
+      })
+  @GetMapping("/course-details/{courseId}")
+  @ResponseStatus(HttpStatus.OK)
+  public CourseDetails getCourseDetailsByCourseId(
+      @PathVariable(value = "courseId") @NotNull String courseId) {
+    return courseDetailService.getCourseDescriptionById(courseId);
+  }
 }
