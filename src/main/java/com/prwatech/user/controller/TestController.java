573c1580ba3127688cb6f00cb3aee807b6f89448
Manish <manishsinghonline2@gmail.com>
2023-05-10 15:13:44 +0530

    Manish | test-dev | Added test api.

--- src/main/java/com/prwatech/user/controller/TestController.java
+++ src/main/java/com/prwatech/user/controller/TestController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class TestController {
 
 
