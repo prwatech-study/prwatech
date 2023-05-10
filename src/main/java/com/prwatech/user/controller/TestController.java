ab1ca494f416dbcf174f72fa595dd85134027dc9
Manish <manishsinghonline2@gmail.com>
2023-05-10 15:19:08 +0530

    Manish | test-dev | Added test api for pipline test.

--- src/main/java/com/prwatech/user/controller/TestController.java
+++ src/main/java/com/prwatech/user/controller/TestController.java
@@ -5,10 +5,12 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/public")
 public class TestController {
 
   @ApiOperation(value = "Test hello api", notes = "Test hello api")
