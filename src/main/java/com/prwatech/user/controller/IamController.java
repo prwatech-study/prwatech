55009b725a24f70eb253a17a49451a49f175b4ba
Manish <manishsinghonline2@gmail.com>
2023-05-10 13:46:17 +0530

    Manish | test-dev | Added test api.

--- src/main/java/com/prwatech/user/controller/IamController.java
+++ src/main/java/com/prwatech/user/controller/IamController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
---
+++ src/main/java/com/prwatech/user/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.prwatech.user.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+
+    @ApiOperation(
+            value = "Test hello api",
+            notes = "Test hello api")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Success"),
+                    @ApiResponse(code = 400, message = "Not Available"),
+                    @ApiResponse(code = 401, message = "UnAuthorized"),
+                    @ApiResponse(code = 403, message = "Access Forbidden"),
+                    @ApiResponse(code = 404, message = "Not found"),
+                    @ApiResponse(code = 422, message = "UnProcessable entity"),
+                    @ApiResponse(code = 500, message = "Internal server error"),
+            })
+    @ResponseStatus(value = HttpStatus.OK)
+    @GetMapping("/")
+    public String helloApi(){
+        return "Welcome to Prwatech !";
+    }
+}
