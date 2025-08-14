82c4bde47450a27f1936fe9bce211cb4fa0f6db9
jitendra.chandwan <jitendra.chandwani@infoedge.com>
2025-08-14 18:54:55 +0530

    HOTFIX | Swagger password changed

--- src/main/java/com/prwatech/common/configuration/SecurityConfig.java
+++ src/main/java/com/prwatech/common/configuration/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
             .withUser("jitendra").password("{noop}prwatech1234").roles("SWAGGER")
             .and()
-            .withUser("prwatech").password("{noop}prwatech1234").roles("SWAGGER")
+            .withUser("prwatech").password("{noop}prwatech@2025").roles("SWAGGER")
             .and()
             .withUser("sushant").password("{noop}prwatech1234").roles("SWAGGER");
     }
